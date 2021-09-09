246119e01f500c4804dbd7f441ea3d3bb52386a1
SivadRovert <78452359+SivadRovert@users.noreply.github.com>
2021-09-09 09:52:27 -0700

    Stealing

--- Assets/Scripts/AI Scripts/ThiefPathfinding.cs
+++ Assets/Scripts/AI Scripts/ThiefPathfinding.cs
@@ -8,16 +8,51 @@ public class ThiefPathfinding : MonoBehaviour
 
     public NavMeshAgent Agent;
     public GameObject Target;
+    public float TBD;
+    private float TBDProgress;
+    private bool Busy;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        TBDProgress = TBD;
+
         Target = GameObject.FindGameObjectWithTag("Target");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Agent.SetDestination(Target.transform.position);
+        if (Busy == false)
+        {
+            Agent.SetDestination(Target.transform.position);
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == Target)
+        {
+            print("steal");
+            StealAction();
+
+        }
+    }
+
+    private void StealAction()
+    {
+        if (TBDProgress > 0)
+        {
+            print("Progress");
+            TBDProgress -= Time.deltaTime;
+        }
+        else
+        {
+            TBDProgress = TBD;
+            Destroy(Target);
+        }
+        
     }
 }
