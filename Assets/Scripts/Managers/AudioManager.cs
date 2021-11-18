742dc3793bc78f72805d8e791b5681765aeeffdb
Kilan Larsen <kilanlarsen@gmail.com>
2021-11-17 22:04:30 -0800

    Audio Test Setup

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -9,21 +9,59 @@ public class AudioManager : SingletonPattern<AudioManager>
     public AudioClip dayTrack;
     public AudioClip nightTrack;
 
+    public AudioClip[] testTracks;
+    private int currTrack = 0;
+
     private AudioSource audioSource;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
+    private void Update()
+    {
+        if (PlayerInputs.Instance.Hotkey1)
+            PreviousTrack();
+
+        if (PlayerInputs.Instance.Hotkey2)
+            NextTrack();
+    }
+
     public void PlayDayTrack()
     {
-        audioSource.PlayOneShot(dayTrack);
+        audioSource.clip = dayTrack;
+        audioSource.Play();
+        Debug.Log("Now Playing " + testTracks[currTrack].name);
     }
 
     public void PlayNightTrack()
     {
-        audioSource.PlayOneShot(nightTrack);
+        audioSource.clip = nightTrack;
+        audioSource.Play();
+        Debug.Log("Now Playing " + testTracks[currTrack].name);
+    }
+
+    public void NextTrack()
+    {
+        currTrack++;
+        if (currTrack > testTracks.Length - 1)
+            currTrack = 0;
+
+        audioSource.clip = testTracks[currTrack];
+        audioSource.Play();
+        Debug.Log("Now Playing " + testTracks[currTrack].name);
+    }
+
+    public void PreviousTrack()
+    {
+        currTrack--;
+        if (currTrack < 0)
+            currTrack = testTracks.Length - 1;
+
+        audioSource.clip = testTracks[currTrack];
+        audioSource.Play();
+        Debug.Log("Now Playing " + testTracks[currTrack].name);
     }
 }
